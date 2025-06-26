75d003519c4f77163e83b0a726088612f9fbc14d
shikha <shikhakumari152019@gmail.com>
2025-06-26 09:22:15 +0530

    Update button color for better visibility

--- src/components/SumPage.jsx
+++ src/components/SumPage.jsx
@@ -10,12 +10,12 @@ const SumPage = () => {
     setResult(response)
   }
   return (
-    <div className='min-h-screen w-full bg-blue-950 flex justify-center items-center'>
+    <div className='min-h-screen w-full bg-blue-950 flex justify-center items-center '>
       <div className='flex flex-col w-80 gap-2 bg-white rounded-lg shadow-lg p-10'>
         
           First input:<input className='border-2 rounded-lg p-2' onChange={(e)=>setInput1(e.target.value)}/>
           Second input:<input className='border-2 rounded-lg p-2' onChange={(e)=>setInput2(e.target.value)} />
-          <button className='bg-black text-white p-2 mt-5  rounded-md' type="button" onClick={clickHandler}>Calculate</button>
+          <button className='bg-blue-950 text-white p-2 mt-5  rounded-md' type="button" onClick={clickHandler}>Calculate</button>
 
           Result:<input className='border-2 rounded-lg p-2' value={result} readOnly/>
       </div>
